63029d7aaa0264cf22fd7c7cd6c78f7b741d2945
gkodosis <geo_kont@live.com>
2020-08-21 04:39:32 +0300

    img fixes

--- src/components/head.js
+++ src/components/head.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import config from '@config';
 import favicon from '@images/favicons/favicon.ico';
+import ogImage from '@images/og.png';
 import appleIcon57x57 from '@images/favicons/apple-icon-57x57.png';
 import appleIcon60x60 from '@images/favicons/apple-icon-60x60.png';
 import appleIcon72x72 from '@images/favicons/apple-icon-72x72.png';
@@ -25,7 +26,7 @@ const Head = ({ metadata }) => (
       {metadata.title}
     </title>
     <link rel="shortcut icon" href={favicon} />
-    <link rel="canonical" href="https://gkodosis.com" />
+    <link rel="canonical" href="https://brittanychiang.com" />
 
     <meta name="description" content={metadata.description} />
     <meta name="keywords" content={config.siteKeywords} />
