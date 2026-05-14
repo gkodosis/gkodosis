9f354b430838656610a5cf329f1680af077be072
Georgios Kontosis <geo_kont@live.com>
2026-05-15 02:55:19 +0300

    Revert gatsby-config change

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         icon: 'src/images/logo.png',
       },
     },
-    `gatsby-plugin-remove-serviceworker`,
+    `gatsby-plugin-offline`,
     {
       resolve: 'gatsby-source-filesystem',
       options: {
