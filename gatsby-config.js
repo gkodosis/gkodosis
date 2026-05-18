268955f959b3aadc76cb31ddd66aa9d520743fb0
Georgios Kontosis <geo_kont@live.com>
2026-05-19 02:59:34 +0300

    Replace gatsby-plugin-offline with remove-serviceworker

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         icon: 'src/images/logo.png',
       },
     },
-    `gatsby-plugin-offline`,
+    `gatsby-plugin-remove-serviceworker`,
     {
       resolve: 'gatsby-source-filesystem',
       options: {
